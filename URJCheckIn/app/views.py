911326e403f2c685a8e0aa7d91cf7ee024458a18
jbazaco <bazaco15@gmail.com>
2014-01-03 19:58:46 +0100

    Git inicializado

---
+++ URJCheckIn/app/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Context, RequestContext
+from django.shortcuts import render_to_response
+
+
+def notFound(request):
+	return render_to_response('main.html', {'htmlname': '404.html'},
+		context_instance=RequestContext(request))
+
+def home(request):
+	return render_to_response('main.html', {'htmlname': 'home.html'},
+		context_instance=RequestContext(request))
