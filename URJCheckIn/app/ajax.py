81462bae9bd51ae81b0ca262ac1c9ca589574334
jbazaco <bazaco15@gmail.com>
2014-01-30 14:04:34 +0100

    funciona el borrar clase en su primera fase

--- URJCheckIn/app/ajax.py
+++ URJCheckIn/app/ajax.py
@@ -3,17 +3,26 @@ from dajaxice.decorators import dajaxice_register
 from django.template import loader, Context
 from forms import ReviewClassForm, ProfileEditionForm
 
-from django.template.loader import render_to_string
+from django.utils.datastructures import MultiValueDictKeyError
 
 @dajaxice_register
 def sayhello(request):
 	return simplejson.dumps({'message':'Hello World'})
 
 @dajaxice_register(method='GET')
-def profile(request, iduser):#TODO, no renderiza bien...
+def profile(request, iduser):
 	templ = loader.get_template('profile.html')
 	cont = Context({'user': {'name':iduser, 'student': False, 'id':iduser}, 
 					'classes': [{'id':'idclase1', 'name':'clase1'}, {'id':'idclase2', 'name':'clase2'}],
 					'form': ProfileEditionForm()})
 	html = templ.render(cont)
 	return simplejson.dumps({'#mainbody':html, 'url': '/profile/view/'+iduser})
+
+
+@dajaxice_register(method='POST')
+def process_class(request, idclass):
+	if request.method == "POST":
+		print "bbb:" + idclass
+		qd= request.POST
+		print qd
+	return simplejson.dumps(['#xc_'+idclass])
--- URJCheckIn/static/javascript/extra_class.js
+++ URJCheckIn/static/javascript/extra_class.js
@@ -1,18 +1,28 @@
 function deleteClass(id) {
-	$.post("http://" + document.location.host + "/class/" + id, "action=delete", //TODO mirar si es esa URL
+	console.log("AAAAAAAA");
+	Dajaxice.app.process_class(removeFromDOM, {'idclass':id});
+	console.log("BBB");
+	/*$.post("http://" + document.location.host + "/class/" + id, "action=delete", //TODO mirar si es esa URL
 				function() { removeFromDOM("#xc_" + id); })
-				.fail(function() { alert( "Error al eliminar clase" ); });
+				.fail(function() { alert( "Error al eliminar clase" ); });*/
 }
 
 function uncheckClass(id) {
-	$.post("http://" + document.location.host + "/class/" + id, "action=uncheck", //TODO mirar si es esa URL
+	Dajaxice.app.process_class(removeFromDOM, {'idclass':id});
+	/*$.post("http://" + document.location.host + "/class/" + id, "action=uncheck", //TODO mirar si es esa URL
 				function() { removeFromDOM("#xc_" + id); })
-				.fail(function() { alert( "Error al desapuntarse del seminario" ); });
+				.fail(function() { alert( "Error al desapuntarse del seminario" ); });*/
 }
 
 ////////////////////LLEVAR A UN JS GENERICO
 //Elimina el elemento con la id=id del arbol DOM
-function removeFromDOM(id) {
-	var elem = $(id);
-	if (elem) elem.remove();
+function removeFromDOM(/*id*/data) {
+	console.log("CCCC");
+	/*var elem = $(id);
+	if (elem) elem.remove();*/
+	for (i=0; i<data.length; i++) {
+		console.log($(data[i]));
+		$(data[i]).remove();
+		console.log("removed");
+	}
 }
