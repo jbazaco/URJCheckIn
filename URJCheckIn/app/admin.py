3dd9a740e0655b66c969cc9ed8407e2029ec23a2
jbazaco <bazaco15@gmail.com>
2014-02-12 14:16:09 +0100

    añadidas algunas tablas en models.py (algunas de ellas puede que estén incompletas aún)

---
+++ URJCheckIn/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Degree, Subject, Room, UserProfile, TeacherProfile
+
+admin.site.register(Degree)
+admin.site.register(Subject)
+admin.site.register(Room)
+admin.site.register(UserProfile)
+admin.site.register(TeacherProfile)
--- URJCheckIn/app/models.py
+++ URJCheckIn/app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Degree(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	def __unicode__(self):
+		return self.name
+
+class Subject(models.Model):
+	name = models.CharField(max_length=100)
+	degree = models.ForeignKey(Degree)
+	
+	class Meta:
+		unique_together = ("name", "degree")
+	
+	def __unicode__(self):
+		return u"%s %s" % (self.name, self.degree)
+
+class Room(models.Model):
+	room = models.CharField(max_length=20)
+	building = models.CharField(max_length=20)
+	centre_longitude = models.IntegerField()
+	centre_latitude = models.IntegerField()
+	radius = models.IntegerField()
+
+	class Meta:
+		unique_together = ("room", "building")
+
+	def __unicode__(self):
+		return u"room %i" % (self.id)
+
+class UserProfile(models.Model):
+	user = models.CharField(max_length=20) #models.OneToOneField(User)
+	description = models.CharField(max_length=200, blank=True)
+	subjects = models.ManyToManyField(Subject, blank=True)
+	
+	def __unicode__(self):
+		return self.user
+
+class StudentProfile(UserProfile):
+	degree = models.ForeignKey(Degree)
+	start_date = models.DateField() #cuando comenzo los estudios
+	
+class TeacherProfile(UserProfile):
+	degrees = models.ManyToManyField(Degree, blank=True) #en los que imparte clase
+	#TODO si se introduce una clase(+profesor) se debe poner el degree si no estaba
+	
+	
+
