75f8dfa14cccd8bb768c8c6f0bf8c5d5234939dd
jbazaco <bazaco15@gmail.com>
2014-02-14 19:00:43 +0100

    se había borrado ReviewClass de models pero seguía en admin. Ya se ha borrado del todo

--- URJCheckIn/app/admin.py
+++ URJCheckIn/app/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from models import Degree, Subject, Room, UserProfile, ClassReview, Lesson, CheckIn, ForumComment, TimeTable
+from models import Degree, Subject, Room, UserProfile, Lesson, CheckIn, ForumComment, TimeTable
 
 admin.site.register(Degree)
 admin.site.register(Subject)
 admin.site.register(Room)
 admin.site.register(UserProfile)
-admin.site.register(ClassReview)#no deberian poder cambiarlo los administradores
 admin.site.register(Lesson)
 admin.site.register(CheckIn)
 admin.site.register(ForumComment)#no deberian poder poner nuevos o modificarlos, solo borrarlos
