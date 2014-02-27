90ef7c905b7c3e027753171f37c2518389846f4a
jbazaco <bazaco15@gmail.com>
2014-02-27 10:55:58 +0100

    modificado MEDIA_URL y STATIC_ROOT, añadido comentario para revisarlo

--- URJCheckIn/URJCheckIn/settings.py
+++ URJCheckIn/URJCheckIn/settings.py
@@ -55,13 +55,13 @@ MEDIA_ROOT = '/userfiles/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/media/'
+STATIC_ROOT = ''#TODO mirar docu
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
