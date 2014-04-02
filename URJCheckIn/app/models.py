1d7e9477d01efd635beabf1f49942de6190ddfd5
jbazaco <bazaco15@gmail.com>
2014-04-02 17:49:05 +0200

    ahora todos los textos aparecen en español

--- URJCheckIn/app/models.py
+++ URJCheckIn/app/models.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -9,19 +10,19 @@ import os
 from django.db.models.signals import post_save
 
 WEEK_DAYS = (
-	('Mon', 'Monday'),
-	('Tue', 'Tuesday'),
-	('Wed', 'Wednesday'),
-	('Thu', 'Thursday'),
-	('Fri', 'Friday'),
-	('Sat', 'Saturday'),
-	('Sun', 'Sunday')
+	('Mon', 'Lunes'),
+	('Tue', 'Martes'),
+	('Wed', 'Miércoles'),
+	('Thu', 'Jueves'),
+	('Fri', 'Viernes'),
+	('Sat', 'Sábado'),
+	('Sun', 'Domingo')
 )
 FIRST_ADMIN_ID = 1
 
 class Degree(models.Model):
 	name = models.CharField(max_length=100, unique=True, verbose_name='nombre')
-	code = models.CharField(max_length=6, unique=True, verbose_name='codigo')
+	code = models.CharField(max_length=6, unique=True, verbose_name='código')
 
 	class Meta:
 		verbose_name='grado'
@@ -33,11 +34,11 @@ class Subject(models.Model):
 	name = models.CharField(max_length=100, verbose_name='nombre')
 	degrees = models.ManyToManyField(Degree, verbose_name='grados')
 	first_date = models.DateField(verbose_name='fecha de inicio')
-	last_date = models.DateField(verbose_name='fecha de finalizacion')
+	last_date = models.DateField(verbose_name='fecha de finalización')
 	is_seminar = models.BooleanField(verbose_name='es seminario', default=False)
 	#util para seminarios, se puede dejar a 0 para clases
 	max_students = models.PositiveIntegerField(verbose_name='plazas', default=0)
-	description = models.TextField(max_length=200, blank=True, verbose_name='descripcion')
+	description = models.TextField(max_length=200, blank=True, verbose_name='descripción')
 	creator = models.ForeignKey(User, verbose_name='creador', default=FIRST_ADMIN_ID)
 
 	class Meta:
@@ -50,7 +51,7 @@ class Subject(models.Model):
 		super(Subject, self).clean()
 		if self.first_date and self.last_date:
 			if (self.first_date > self.last_date):
-				raise ValidationError('First_date can not be greater than last_date')
+				raise ValidationError('La fecha de inicio no puede ser posterior a la de finalización')
 
 	def n_students(self):
 		return self.userprofile_set.filter(is_student=True).count()
@@ -128,7 +129,7 @@ def remove_if_exists(name):
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, verbose_name='usuario')
 	photo = models.ImageField(upload_to=user_image_path, blank=True)#Poner una por defecto (la tipica silueta)
-	description = models.TextField(max_length=200, blank=True, verbose_name='descripcion')
+	description = models.TextField(max_length=200, blank=True, verbose_name='descripción')
 	subjects = models.ManyToManyField(Subject, blank=True, verbose_name='asignatura')
 	degrees = models.ManyToManyField(Degree, blank=True, verbose_name='grados')
 	#TODO si se introduce una asignatura(+profesor) se debe poner el degree si no estaba
@@ -152,12 +153,12 @@ def get_rand_string():
 
 class Lesson(models.Model):
 	start_time =  models.DateTimeField(verbose_name='hora de inicio')
-	end_time = models.DateTimeField(verbose_name='hora de finalizacion')
+	end_time = models.DateTimeField(verbose_name='hora de finalización')
 	subject = models.ForeignKey(Subject, verbose_name='asignatura')
 	room =	models.ForeignKey(Room, verbose_name='aula')#TODO on_delete funcion para buscar otra aula
 	is_extra = models.BooleanField(default=False, verbose_name='es clase extra')
 	done = models.BooleanField(verbose_name='realizada', default=False)
-	codeword = models.CharField(max_length=20, verbose_name='codigo', default=get_rand_string)
+	codeword = models.CharField(max_length=20, verbose_name='código', default=get_rand_string)
 	students_counted = models.PositiveIntegerField(verbose_name='alumnos contados', default=0)
 	
 	class Meta:
@@ -170,9 +171,9 @@ class Lesson(models.Model):
 		super(Lesson, self).clean()
 		if self.start_time and self.end_time:
 			if self.start_time < timezone.now():
-				raise ValidationError('Start_time must be greater than now')
+				raise ValidationError('La hora de inicio debe ser posterior a este momento')
 			if self.start_time >= self.end_time:
-				raise ValidationError('End_time must me greater than start_time')
+				raise ValidationError('La hora de finalización debe ser posterior a la de inicio')
 			#Para evitar solapamiento de clases
 			lesson_same_time = Lesson.objects.exclude(
 						start_time__gte=self.end_time
@@ -183,11 +184,11 @@ class Lesson(models.Model):
 					)
 			try:
 				if lesson_same_time.filter(subject=self.subject):
-					raise ValidationError('The lesson can not coincide with ' +
-											'another of the same subject')
+					raise ValidationError('La clase no puede solaparse con otra ' +
+											'de la misma asignatura')
 				if lesson_same_time.filter(room=self.room):
-					raise ValidationError('The lesson can not coincide with ' +
-											'another in the same room')
+					raise ValidationError('La clase no puede solaparse con otra ' +
+											'en el mismo aula')
 			except (Subject.DoesNotExist, Room.DoesNotExist):
 				pass
 
@@ -210,7 +211,7 @@ class Lesson(models.Model):
 class CheckIn(models.Model):
 	user = models.ForeignKey(User, verbose_name='usuario')
 	lesson = models.ForeignKey(Lesson, verbose_name='clase')
-	mark = models.PositiveIntegerField(validators=[MaxValueValidator(5)], verbose_name='puntuacion', blank=True)
+	mark = models.PositiveIntegerField(validators=[MaxValueValidator(5)], verbose_name='puntuación', blank=True)
 	longitude = models.FloatField(verbose_name='longitud', blank=True, null=True)
 	latitude = models.FloatField(verbose_name='latitud', blank=True, null=True)
 	codeword = models.CharField(max_length=20, verbose_name='codigo', blank=True)
@@ -225,10 +226,10 @@ class CheckIn(models.Model):
 	def clean(self):
 		super(CheckIn, self).clean()
 		if not (self.codeword or self.longitude):
-			raise ValidationError('Tienes que enviar el codigo o tu localizacion para hacer CheckIn')
+			raise ValidationError('Tienes que enviar el código o tu localización para hacer CheckIn')
 		if self.codeword:
 			if self.codeword != self.lesson.codeword:
-				raise ValidationError('Ese codigo no se corresponde con el de la clase actual')
+				raise ValidationError('Ese código no se corresponde con el de la clase actual')
 
 
 def check_lesson_done(sender, instance, **kwargs):
@@ -276,7 +277,7 @@ class Timetable(models.Model):
 	subject = models.ForeignKey(Subject, verbose_name='asignatura')
 	day = models.CharField(max_length=3, choices=WEEK_DAYS)
 	start_time =  models.TimeField(verbose_name='hora de inicio')
-	end_time = models.TimeField(verbose_name='hora de finalizacion')
+	end_time = models.TimeField(verbose_name='hora de finalización')
 	room =	models.ForeignKey(Room, verbose_name='aula')#TODO on_delete decidir que hago
 	#Posible solucion poner edificio por si el aula no esta disponible
 	#TODO No se si poner profesor o nada y que sean todos los de la asignatura
@@ -291,7 +292,7 @@ class Timetable(models.Model):
 		super(Timetable, self).clean()
 		if self.start_time and self.end_time and self.day:
 			if (self.start_time >= self.end_time):
-				raise ValidationError('End_time must me greater than start_time')
+				raise ValidationError('La hora de finalización debe ser posterior a la de inicio')
 			#Para evitar solapamiento de clases
 			timetables_same_time = Timetable.objects.filter(
 							day=self.day
@@ -304,11 +305,11 @@ class Timetable(models.Model):
 						)
 			try:
 				if timetables_same_time.filter(subject=self.subject):
-					raise ValidationError('The timetable can not coincide with \
-											another of the same subject')
+					raise ValidationError('El horario no puede solaparse con otro ' +
+											'de la misma asignatura')
 				if timetables_same_time.filter(room=self.room):
-					raise ValidationError('The timetable can not coincide with \
-											another in the same room')
+					raise ValidationError('El horario no puede solaparse con otro ' +
+											'en la misma aula')
 			except (Subject.DoesNotExist, Room.DoesNotExist):
 				pass
 	#TODO despues de guardar hay que crear las clases
