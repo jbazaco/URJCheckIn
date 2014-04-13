a33bc05315ebb9f7ba0f44876bcce5a0689a256b
jbazaco <bazaco15@gmail.com>
2014-04-13 12:55:18 +0200

    solucionado problema con la localización en firefox (que no funciona bien en muchas versiones de firefox) añadiendo un timeout a getCurrentPosition. Además, ahora en los errores de geolocalización en vez de enviarlo directamente sin localización, aparece un botón para hacerlo, por si se quiere reintentar con localización o no

--- URJCheckIn/static/javascript/location.js
+++ URJCheckIn/static/javascript/location.js
@@ -1,17 +1,21 @@
 
 $(document).ready(function() {
 	$('#mainbody').delegate('#checkinbox', 'submit', checkIn);
+	$('#mainbody').delegate('#without_geolocation', 'click', sendWithoutGeolocation);
 })
 
 var checkin_alert_class = 'alert_checkin';
 
+/* Envia el formulario sin la localizacion si el navegador no tiene localizacion
+	o localiza al usuario y envia el formulario con su posicion */
 function checkIn(event) {
 	event.preventDefault();
 	$('.'+checkin_alert_class).remove();
 	$('#result_checkin').html("");
 	$('#loading_page').css('display','inline');
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(sendLocation, geolocationError, {enableHighAccuracy: true});
+		navigator.geolocation.getCurrentPosition(sendLocation, geolocationError, 
+		{enableHighAccuracy: true, timeout: 5000});
 	} else {
 		$.post($('#checkinbox').attr('action'), $('#checkinbox').serialize(), checkinDone);
 	}
@@ -54,11 +58,30 @@ function checkinDone(data) {
 	localizacion o es imposible localizarlo, por lo que tiene que poder realizar el checkin de 
 	alguna manera */
 function geolocationError(error) {
-	alertBefore('Error #' + error.code + ' ' + error.message + 
-				' (check in enviado sin geolocalizaci&oacute;n)', 
-				'#checkinbox button', checkin_alert_class, 'warning', '#checkinbox');
-	$.post($('#checkinbox').attr('action'), $('#checkinbox').serialize(), checkinDone);
+	var error_msg;
+	if (error.code == 1) {
+		error_msg = 'Acceso a la localizaci&oacute;n denegado';
+	} else if (error.code == 2) {
+		error_msg = 'Posici&oacute;n no disponible';
+	} else if (error.code == 3) {
+		error_msg = 'El tiempo de localizaci&oacute;n excedi&oacute; el l&iacute;mite';
+	} else {
+		error_msg = error.message;
+	}
+	error_msg += ' <a href="#" id="without_geolocation" class="btn btn-info">' + 
+				'<span class="glyphicon glyphicon-circle-arrow-right"></span> ' +
+				'Enviar sin localizaci&oacute;n</a>';
+	alertBefore(error_msg, '#checkinbox button', checkin_alert_class, 'warning', '#checkinbox');
 	$('#loading_page').hide();
+}
+
+/* Envia el formulario sin intentar localizar al usuario */
+function sendWithoutGeolocation(event) {
+	event.preventDefault();
+	$('.'+checkin_alert_class).remove();
+	$('#result_checkin').html("");
+	$('#loading_page').css('display','inline');
+	$.post($('#checkinbox').attr('action'), $('#checkinbox').serialize(), checkinDone);
 }
 
 /* funcion cogida de https://github.com/LazarSoft/jsqrcode */
